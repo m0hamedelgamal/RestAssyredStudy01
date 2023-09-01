34a60a7bf4bdf60f8b17775058ca191584e6b317
m0hamedelgamal <mohamed.ali.elgamal@gmail.com>
2023-09-01 11:15:17 +0300

    Adding the assertions

--- src/test/java/hatemCourse/testAssertion.java
+++ src/test/java/hatemCourse/testAssertion.java
@@ -1,12 +1,15 @@
 package hatemCourse;
+
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
-import static io.restassured.RestAssured.*;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class testAssertion {
     @Test
-    public void validateThestatusCode() {
+    public void assertionWith_ThestatusCode() {
 
         baseURI = "https://64ea0695bf99bdcc8e67378a.mockapi.io/api/v1";
         given().
@@ -17,7 +20,7 @@ public class testAssertion {
     }
 
     @Test
-    public void validatetheEqualTo() {
+    public void assertionWith_EqualTo() {
 
         baseURI = "https://64ea0695bf99bdcc8e67378a.mockapi.io/api/v1";
         Response requestResponse =
@@ -27,7 +30,7 @@ public class testAssertion {
     }
 
     @Test
-    public void validatetheHasItem() {
+    public void assertionWith_HasItem() {
 
         baseURI = "https://64ea0695bf99bdcc8e67378a.mockapi.io/api/v1";
         Response requestResponse = given().baseUri(baseURI).
@@ -38,13 +41,102 @@ public class testAssertion {
     }
 
     @Test
-    public void validatetheResponseHasItems() {
-
+    public void assertionWith_hasItems() {
         baseURI = "https://64ea0695bf99bdcc8e67378a.mockapi.io/api/v1";
         Response requestResponse = given().baseUri(baseURI).
                 when().get("/userinfo");
         requestResponse.then().assertThat().
-                body("fname", hasItems("fname 2", "fname 1"));
+                body("id", hasItems("1"));
+    }
 
+    @Test
+    public void assertionWith_Not() {
+        baseURI = "https://64ea0695bf99bdcc8e67378a.mockapi.io/api/v1";
+        Response requestResponse = given().baseUri(baseURI).
+                when().get("/userinfo");
+        requestResponse.then().assertThat().
+                body("fname", not(hasItems("fname a2", "fname 11")));
+    }
+
+    @Test
+    public void assertionWith_Contains() {
+        baseURI = "https://64ea0695bf99bdcc8e67378a.mockapi.io/api/v1";
+        Response requestResponse = given().baseUri(baseURI).
+                when().get("/userinfo");
+        requestResponse.then().assertThat().
+                body("fname", contains("fname 1", "fname 2", "fname 3", "fname 4"));
+    }
+
+    @Test
+    public void assertionWith_containsInAnyOrder() {
+        baseURI = "https://64ea0695bf99bdcc8e67378a.mockapi.io/api/v1";
+        Response requestResponse = given().baseUri(baseURI).
+                when().get("/userinfo");
+        requestResponse.then().assertThat().
+                body("fname", containsInAnyOrder("fname 3", "fname 4", "fname 1", "fname 2"));
+    }
+
+    @Test
+    public void assertionWith_Empty() {
+        baseURI = "https://64ea0695bf99bdcc8e67378a.mockapi.io/api/v1";
+        Response requestResponse = given().baseUri(baseURI).
+                when().get("/userinfo");
+        requestResponse.then().assertThat().
+                body("id", is(empty()));
+    }
+
+    @Test
+    public void assertionWith_notEmpty() {
+        baseURI = "https://64ea0695bf99bdcc8e67378a.mockapi.io/api/v1";
+        Response requestResponse = given().baseUri(baseURI).
+                when().get("/userinfo");
+        requestResponse.then().assertThat().body("id", is(not(empty())));
+    }
+
+    @Test
+    public void assertionWith_hasSize() {
+        baseURI = "https://64ea0695bf99bdcc8e67378a.mockapi.io/api/v1";
+        Response requestResponse = given().baseUri(baseURI).
+                when().get("/userinfo");
+        requestResponse.then().assertThat().body("id", hasSize(4));
+//Same can be use instead the equal to
+        requestResponse.then().assertThat().body("id.size()", equalTo(4));
+    }
+
+    @Test
+    public void assertionWith_everyItem() {
+        baseURI = "https://64ea0695bf99bdcc8e67378a.mockapi.io/api/v1";
+        Response requestResponse = given().baseUri(baseURI).
+                when().get("/userinfo");
+        requestResponse.then().assertThat().
+                body("createdAt", everyItem(startsWith("2023")));
+    }
+
+    @Test
+    public void assertionWith_hasKey() {
+        baseURI = "https://64ea0695bf99bdcc8e67378a.mockapi.io/api/v1";
+        Response requestResponse =
+                given().baseUri(baseURI).
+                        when().get("/userinfo");
+        requestResponse.then().assertThat().
+                body("[0]", hasKey("createdAt"));
+    }
+
+    @Test
+    public void assertionWith_hasValue() {
+        baseURI = "https://64ea0695bf99bdcc8e67378a.mockapi.io/api/v1";
+        Response requestResponse = given().baseUri(baseURI).
+                when().get("/userinfo");
+        requestResponse.then().assertThat().
+                body("[0]", hasValue("Indonesia"));
+    }
+
+    @Test
+    public void assertionWith_hasEntry() {
+        baseURI = "https://64ea0695bf99bdcc8e67378a.mockapi.io/api/v1";
+        Response requestResponse = given().baseUri(baseURI).
+                when().get("/userinfo");
+        requestResponse.then().assertThat().
+                body("[0]", hasEntry("id", "1"));
     }
 }
